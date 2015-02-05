8dfe60aaf858f76749f3486bf0e8f89891304034
Majorbummer <mlaver91589@gmail.com>
2015-02-05 11:58:33 -0600

    Created AccountModel for Account management
      >LoginViewModel -- holds user entry from the Log in page
      >ManageUserModel -- future model for resetting passwords
      >RegisterViewModel -- Future model for Admin to create users
      >EditUserViewModel -- Future model for Admin to edit users
    Created LogIn View
    Created AccountController
      >returns login view
      >checks for login validation and sets cookie
    Changed _Layout to give log out functionality
    Added authentication to Web.config
    Changed the default route to the LogIn page

---
+++ Clock/Controllers/AccountController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Clock.DAL;
+using Clock.Models;
+using System.Web.Security;
+
+namespace Clock.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        //pulls LogIn View
+        [HttpGet]
+        public ActionResult LogIn()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult LogIn(Models.LoginViewModel user, string returnUrl)
+        {
+            bool _LogInFlag;
+            if (ModelState.IsValid)
+            {
+                //calls function to check if username and password are correct
+                _LogInFlag = IsValid(user.Username, user.Password);
+
+                if (_LogInFlag == true)
+                {
+                    //set authentication cookie and redirect
+                    FormsAuthentication.SetAuthCookie(user.Username, false);
+                    return RedirectToAction("Index","Home");
+                }
+
+                else
+                {
+                    //error if failed
+                    ModelState.AddModelError("", "Invalid Username or Password.");
+                }
+            }
+           
+
+            // It should not get this far
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("LogIn", "Account");
+        }
+
+
+
+        //Checs username and password
+        private bool IsValid(string username, string password)
+        {
+
+            bool isValid = false;
+
+            using(var db = new ClockContext())
+            {
+                var user = db.Users.FirstOrDefault(u => u.Username == username);
+
+                if (user != null)
+                {
+
+                    if (user.Password == password)
+                    {
+                        isValid = true;
+                    }
+                }
+
+
+            }
+
+            return isValid;
+        }
+    }
+
+    
+
+
+
+}
---
+++ Clock/Models/AccountModel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Clock.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+
+    public class ManageUserViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current password")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage =
+            "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage =
+            "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        public int RoleId { get; set; }
+        public virtual Role Role { get; set; }
+        public virtual ICollection<TimeEntry> TimeEntries { get; set; }
+
+    }
+
+    public class EditUserViewModel
+    {
+        public EditUserViewModel() { }
+
+        // Allow Initialization with an instance of RegisterViewUser(not tested)
+        public EditUserViewModel(User user)
+        {
+            this.UserName = user.Username;
+            this.FirstName = user.Firstname;
+            this.LastName = user.Lastname;
+
+        }
+
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        public int RoleId { get; set; }
+        public virtual Role Role { get; set; }
+        public virtual ICollection<TimeEntry> TimeEntries { get; set; }
+    }
+}
